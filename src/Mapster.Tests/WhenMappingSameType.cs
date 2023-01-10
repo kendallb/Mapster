90571d08e9df6b1794cdf7dc6b9341b07a36eb68
Kendall Bennett <kendallb@amain.com>
2023-01-10 18:49:58 -0500

    Added failing test

---
+++ src/Mapster.Tests/WhenMappingSameType.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+
+namespace Mapster.Tests
+{
+    [TestClass]
+    public class WhenMappingSameType
+    {
+        [TestMethod]
+        public void Should_Return_Src_Instance()
+        {
+            TypeAdapterConfig<SimplePoco, SimplePoco>.NewConfig()
+                .Compile();
+
+            var poco = new SimplePoco
+            {
+                Id = Guid.NewGuid(),
+                Name1 = "Name1",
+            };
+
+            var dto = poco.Adapt<SimplePoco, SimplePoco>();
+            dto.ShouldBe(poco);
+        }
+
+        [TestMethod]
+        public void Should_Return_Src_Instance_Inferred_Destination_Type()
+        {
+            TypeAdapterConfig<SimplePoco, SimplePoco>.NewConfig()
+                .Compile();
+
+            var poco = new SimplePoco
+            {
+                Id = Guid.NewGuid(),
+                Name1 = "Name1",
+            };
+
+            var dto = poco.Adapt<SimplePoco>();
+            dto.ShouldBe(poco);
+        }
+
+        #region test classes
+        public class SimplePoco
+        {
+            public Guid Id { get; set; }
+            public string Name1 { get; set; }
+        }
+        #endregion
+    }
+}
